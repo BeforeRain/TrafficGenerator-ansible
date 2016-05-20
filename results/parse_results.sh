2a61af9646b6d97f2a17e01694f39cf7878f4b2f
Weiyu Zhang <weiyuzwy@gmail.com>
2016-05-20 22:38:15 +0800

    Fetch flow info from every end host and parse global results

--- results/parse_results.sh
+++ results/parse_results.sh
@@ -2,6 +2,7 @@
 echo >  flows
 find . -name flows.txt -exec cat {} \; > flows
 
-python result.py flows > final_results
+echo "Global results:" > final_results
+python result.py flows >> final_results
 
 find . -name results  -exec echo -e "\n" {} \; >> final_results  -exec cat {} \; >> final_results
